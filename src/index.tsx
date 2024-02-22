9cd9dbfd7e09bea77d57c1360e47fdbe7945b0c6
RandyYanish <rtaylorfargo@gmail.com>
2024-02-22 13:40:42 -0800

    Root

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
 
+// Pages
+// import {
+//   Root,
+// } from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  // {
+  //   path: "/documentation",
+  //   element: <Docs />,
+  // },
+  // {
+  //   path: "/auth",
+  //   element: <Auth />,
+  // }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
---
+++ src/pages/Root.tsx
@@ -0,0 +1,9 @@
+function Root() {
+  return (
+    <div>
+      Hellow World!
+    </div>
+  );
+}
+
+export default Root;
